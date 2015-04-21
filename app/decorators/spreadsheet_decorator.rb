9cf6e8408fd57214afe3029a4f562bc101efb50a
Adam Nowak <adam.nowak@netguru.co>
2015-04-21 10:40:35 +0200

    spreadsheet view

--- app/decorators/spreadsheet_decorator.rb
+++ app/decorators/spreadsheet_decorator.rb
@@ -1,3 +1,17 @@
 class SpreadsheetDecorator < Draper::Decorator
   delegate :title, :id, :human_url
+
+  def available_columns
+    worksheet_data.first[1..-1]
+  end
+
+  def row_identifiers
+    worksheet_data[1..-1].map(&:first)
+  end
+
+  private
+
+  def worksheet_data
+    @worksheet_data ||= object.worksheets[0].rows
+  end
 end
