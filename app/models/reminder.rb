b063bd9dd5cb21b6cd5d717d1df6be9ff14cebbb
Adam Nowak <adam.nowak@netguru.co>
2015-04-25 15:10:29 +0200

    validate reminder name length

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,5 +1,5 @@
 class Reminder < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
   has_many :project_checks
   has_many :projects, through: :project_checks
 end
