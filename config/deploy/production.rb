fd3fdb467f3a338997ad9fe1a44ccdebe0ba0a28
Adam Nowak <adam.nowak@netguru.co>
2015-04-29 17:02:18 +0200

    temp ruby version

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,2 @@
 server ENV["REMINDERS_SERVER_HOST"], user: ENV["REMINDERS_SERVER_USER"], roles: %w(web app db)
 server ENV["REMINDERS_SERVER_HOST_2"], user: ENV["REMINDERS_SERVER_USER"], roles: %w(web app)
-set :branch, "production"
