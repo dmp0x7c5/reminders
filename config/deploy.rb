04e78d340da6370d84a93d51f7cd1b2661592ded
Adam Nowak <adam.nowak@netguru.co>
2015-04-29 17:03:50 +0200

    change sec_config to secrets

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,6 @@ set :application, "reminders"
 set :repo_url,  "git://github.com/netguru/reminders.git"
 set :deploy_to, ENV["REMINDERS_DEPLOY_PATH"]
 
-set :linked_files, %w(config/database.yml config/sec_config.yml)
+set :linked_files, %w(config/database.yml config/secrets.yml)
 
 set :linked_dirs, %w(bin log tmp vendor/bundle public/uploads)
