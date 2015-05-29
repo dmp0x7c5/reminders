ab48d517e3aec70f0b43ae072b19acbc2ebc2b24
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-05-29 13:24:59 +0200

    fetch done project checks (ones that have last_check_date)

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,7 @@ class Project < ActiveRecord::Base
   validates :name, presence: true
   has_many :project_checks, dependent: :destroy
   has_many :reminders, through: :project_checks
+  has_many :checked_reviews,
+           -> { where.not(last_check_date: nil).order(created_at: :desc) },
+           class_name: "ProjectCheck"
 end
