9085f5c5dec54166d39b00b7f45123803c6f639e
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-01 11:08:42 +0200

    projects per reminder

--- app/models/project_check.rb
+++ app/models/project_check.rb
@@ -2,4 +2,6 @@ class ProjectCheck < ActiveRecord::Base
   belongs_to :project
   belongs_to :reminder
   belongs_to :last_check_user, class: User
+
+  scope :for_reminder, -> (reminder_id) { where(reminder_id: reminder_id).order(created_at: :desc) }
 end
