d9fcf2f00a447ea690276949ff2c7d1cbb6a620e
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-01 11:08:16 +0200

    add decorated check collections in project decorator

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -13,6 +13,14 @@ class ProjectDecorator < Draper::Decorator
     ::ProjectCheckDecorator.decorate_collection object.checked_reviews
   end
 
+  def checks
+    ::ProjectCheckDecorator.decorate_collection object.project_checks
+  end
+
+  def checks_per_reminder(reminder_id)
+    ::ProjectCheckDecorator.decorate_collection object.project_checks.order(created_at: :desc).select { |c| c.reminder_id == reminder_id }
+  end
+
   def has_checked_reviews?
     checked_reviews.any?
   end
