58cbea793efc171aada544fe9459da14a0755858
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-01 12:18:33 +0200

    guard fix, line was too long

--- app/models/project_check.rb
+++ app/models/project_check.rb
@@ -3,5 +3,8 @@ class ProjectCheck < ActiveRecord::Base
   belongs_to :reminder
   belongs_to :last_check_user, class: User
 
-  scope :for_reminder, -> (reminder_id) { where(reminder_id: reminder_id).order(created_at: :desc) }
+  scope :for_reminder, lambda  { |reminder_id|
+    where(reminder_id: reminder_id)
+      .order(created_at: :desc)
+  }
 end
