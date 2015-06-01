393acc80e5e4b48518d0ccd40f4fc58f6ac6330d
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-01 18:27:54 +0200

    guard fix

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -7,7 +7,7 @@ class RemindersController < ApplicationController
     ReminderDecorator::Base.decorate_collection reminders_repository.all
   end
   expose(:reminder) { reminders_repository.find(params[:id]) }
-  expose(:reminder2) {reminders_repository.find(params[:id]) }
+  expose(:reminder2) { reminders_repository.find(params[:id]) }
   expose(:project_checks_repository) { ProjectChecksRepository.new }
   expose(:project_checks) do
     checks = project_checks_repository.for_reminder(reminder)
