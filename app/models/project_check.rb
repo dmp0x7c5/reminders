d5398743d5e1cafcf61680e90e16c03048925851
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-03 10:29:31 +0200

    add association, set default order

--- app/models/project_check.rb
+++ app/models/project_check.rb
@@ -3,4 +3,5 @@ class ProjectCheck < ActiveRecord::Base
   belongs_to :reminder
   belongs_to :last_check_user, class: User
 
+  has_many :check_assignments, -> { order created_at: :desc }, dependent: :destroy
 end
