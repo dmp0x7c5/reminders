c19469923f40f024c352ee006d634bc89bd95dc1
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-08 10:24:58 +0200

    check assignments use case, create class

---
+++ app/use_cases/check_assignments/create.rb
@@ -0,0 +1,29 @@
+module CheckAssignments
+  class Create
+    attr_reader :checker, :assignments_repository, :project_check
+    private :checker, :assignments_repository, :project_check
+
+    def initialize(args)
+      @checker = args.fetch(:checker)
+      @project_check = args.fetch(:project_check)
+      @assignments_repository = CheckAssignmentsRepository.new
+    end
+
+    def call
+      create_assignment
+    end
+
+    private
+
+    def create_assignment
+      assignments_repository.add(prepare_attributes)
+    end
+
+    def prepare_attributes
+      {
+        user_id: checker.id,
+        project_check_id: project_check.id,
+      }
+    end
+  end
+end
