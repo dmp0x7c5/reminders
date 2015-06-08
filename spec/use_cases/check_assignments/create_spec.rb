90fe78aaec5799e70a3bb36ce98f383af746d1b3
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-08 11:22:04 +0200

    spec for CheckAssignments::Create

---
+++ spec/use_cases/check_assignments/create_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe CheckAssignments::Create do
+  let(:service) { described_class.new(parameters) }
+  let(:parameters) do
+    {
+      checker: checker,
+      project_check: project_check,
+    }
+  end
+  let(:assignments_repo) { CheckAssignmentsRepository.new }
+
+  describe "#call" do
+    let(:checker) { create(:user) }
+    let(:project_check) { create(:project_check) }
+
+    it "creates new check assignment" do
+      service.call
+      expect(assignments_repo.latest_assignment(project_check))
+        .not_to be nil
+    end
+
+    it "returns new check assignment" do
+      expect(service.call).to be_an_instance_of CheckAssignment
+    end
+  end
+end
