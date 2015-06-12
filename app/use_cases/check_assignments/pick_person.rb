efc2e51a81ed6793702e924562d32699ca8bbf19
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-12 13:00:07 +0200

    pick person is part of check assignments module

--- app/use_cases/check_assignments/pick_person.rb
+++ app/use_cases/check_assignments/pick_person.rb
@@ -1,19 +1,21 @@
-class PickCheckerService
-  attr_reader :users_repository, :latest_checker
-  private :users_repository, :latest_checker
+module CheckAssignments
+  class PickPerson
+    attr_reader :users_repository, :latest_checker
+    private :users_repository, :latest_checker
 
-  def initialize(data)
-    @users_repository = UsersRepository.new
-    @latest_checker = data.fetch(:latest_checker)
-  end
+    def initialize(data)
+      @users_repository = UsersRepository.new
+      @latest_checker = data.fetch(:latest_checker)
+    end
 
-  def call
-    available_users.sample(1).first
-  end
+    def call
+      available_users.sample(1).first
+    end
 
-  private
+    private
 
-  def available_users
-    users_repository.all.to_a - [latest_checker]
+    def available_users
+      users_repository.all.to_a - [latest_checker]
+    end
   end
 end
