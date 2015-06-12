b073f3603705a5a1d5b271e4a2ff6f8010ef359f
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-12 14:27:39 +0200

    test resolve action

--- spec/use_cases/check_assignments/resolve_action_spec.rb
+++ spec/use_cases/check_assignments/resolve_action_spec.rb
@@ -1,7 +1,24 @@
 require "rails_helper"
 
 describe CheckAssignments::ResolveAction do
-  let(:service) { described_class.new(assignment: assignment) }
+  let(:service) do
+    described_class.new(assignment: assignment,
+                        creator: creator,
+                        completer: completer,
+                       )
+  end
+  let(:creator) do
+    CheckAssignments::CreateCompleted.new(
+      checker: double(:user),
+      project_check: double(:project_check),
+    )
+  end
+  let(:completer) do
+    CheckAssignments::Complete.new(
+      assignment: double(:assignment, project_check: nil),
+      checker: double(:user),
+    )
+  end
 
   describe "#can_create?" do
     context "when assignment is nil" do
@@ -12,7 +29,7 @@ describe CheckAssignments::ResolveAction do
       end
     end
 
-    context "with completed assignment " do
+    context "with completed assignment" do
       let(:assignment) { double(:assignment, completion_date: "2015-06-06") }
 
       it "returns true" do
@@ -25,6 +42,41 @@ describe CheckAssignments::ResolveAction do
 
       it "returns false" do
         expect(service.can_create?).to be false
+      end
+    end
+  end
+
+  describe "#resolve" do
+    let(:assignment) { double(:assignment) }
+
+    before do
+      allow(creator).to receive(:call)
+      allow(completer).to receive(:call)
+    end
+
+    context "when new assignment can be created" do
+      before do
+        allow(service).to receive(:can_create?)
+          .and_return(true)
+      end
+
+      it "makes call to assignment creator" do
+        service.resolve
+        expect(creator).to have_received(:call)
+        expect(completer).not_to have_received(:call)
+      end
+    end
+
+    context "when assignment should be completed" do
+      before do
+        allow(service).to receive(:can_create?)
+          .and_return(false)
+      end
+
+      it "makes call to assignment completer" do
+        service.resolve
+        expect(completer).to have_received(:call)
+        expect(creator).not_to have_received(:call)
       end
     end
   end
