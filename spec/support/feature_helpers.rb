dc42fbf1d6c5e6d02b964839868e8e27db4be8d1
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-02 14:20:21 +0200

    mock login with OmniAuth

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -1,5 +1,10 @@
 module FeatureHelpers
-  def log_in
+  def log_in(user)
+    OmniAuth.config.mock_auth[:google_oauth2] =
+      OmniAuth::AuthHash.new(
+        provider: user.provider,
+        uid: user.uid,
+      )
     visit "auth/google_oauth2"
   end
 end
