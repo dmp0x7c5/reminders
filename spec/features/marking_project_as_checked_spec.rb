7c5cd02d26988e2b438974bac559062d20954d4f
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-02 12:26:45 +0200

    feature spec: checking project that hasn't been checked yet

---
+++ spec/features/marking_project_as_checked_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+feature "marking project as checked" do
+  let(:project) { create(:project) }
+  let(:reminder) { create(:reminder) }
+  let!(:project_check) do
+    create(:project_check,
+           project: project,
+           reminder: reminder,
+          )
+  end
+  let(:user) { User.first }
+
+  before do
+    visit "auth/google_oauth2"
+  end
+
+  context "when project hasn't been checked yet" do
+    scenario "user marks project as checked" do
+      visit reminder_path(reminder)
+      expect(page).not_to have_text user.name
+      expect(page).to have_text "not checked yet"
+
+      click_button("Check!")
+
+      expect(page).not_to have_text "not checked yet"
+      expect(page).to have_text user.name
+      expect(page).to have_text project_check.last_check_date
+    end
+  end
+end
