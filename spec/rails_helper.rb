03fc4b028e67f77dffdba4f215fe0e36cae9e24e
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-02 10:40:49 +0200

    enable OmniAuth test mode

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
+OmniAuth.config.test_mode = true
+
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
