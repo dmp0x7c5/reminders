619fee0bd52f5a6953bb0b120037cde87273d1b3
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-02 10:41:13 +0200

    feature spec for assigning user to perform check

---
+++ spec/features/assigning_person_to_make_check_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+feature "assign user to perform check" do
+  let(:project) { create(:project) }
+  let(:reminder) { create(:reminder) }
+  let!(:project_check) do
+    create(:project_check,
+           project: project,
+           reminder: reminder,
+          )
+  end
+  let(:user) { User.first }
+
+  before do
+    visit "auth/google_oauth2"
+  end
+
+  scenario "there is no user assigned yet" do
+    visit reminder_path(reminder)
+    expect(page).not_to have_text user.name
+    expect(page).to have_button("Pick random")
+
+    click_button("Pick random")
+
+    expect(page).to have_text user.name
+    expect(page).not_to have_button("Pick random")
+  end
+end
