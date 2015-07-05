fe55e36b5c827c674bd258fbfc88278b79bbf49c
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-05 23:32:55 +0200

    site prism - reminder page

---
+++ spec/support/pages/reminders/reminder_page.rb
@@ -0,0 +1,13 @@
+require_relative "../sections/project_checks_table_row.rb"
+
+module Reminders
+  class ReminderPage < SitePrism::Page
+    set_url "/reminders/{reminder_id}"
+    sections :project_rows, ProjectChecksTableRow,
+             ".row .col-xs-12 table.table tbody.project_row tr"
+
+    def first_project
+      project_rows.first
+    end
+  end
+end
