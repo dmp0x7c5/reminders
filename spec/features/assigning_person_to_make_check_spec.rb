a58c12a8001fde6ef04909c86886861946dcd447
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-06 07:16:26 +0200

    failing spec for bugfix

--- spec/features/assigning_person_to_make_check_spec.rb
+++ spec/features/assigning_person_to_make_check_spec.rb
@@ -10,6 +10,7 @@ feature "assign user to perform check" do
           )
   end
   let(:user) { create(:user, uid: "12331", provider: "google_oauth2") }
+  let(:second_user) { create(:user, name: "John Smith") }
   let(:page) { Reminders::ReminderPage.new }
 
   before do
@@ -31,13 +32,26 @@ feature "assign user to perform check" do
     expect(page.first_project)
       .not_to have_button("Pick random")
   end
+
+  scenario "someone has already checked project" do
     visit reminder_path(reminder)
-    expect(page).not_to have_text user.name
-    expect(page).to have_button("Pick random")
+    expect(page.first_project)
+      .not_to have_text user.name
+    expect(page.first_project)
+      .to have_button("Pick random")
 
-    click_button("Pick random")
+    page.first_project.check_button.click
 
-    expect(page).to have_text user.name
-    expect(page).not_to have_button("Pick random")
+    expect(page.first_project.last_checker)
+      .to have_text user.name
+    expect(page.first_project)
+      .not_to have_text second_user.name
+
+    page.first_project.pick_random_button.click
+
+    expect(page.first_project.assigned_reviewer)
+      .to have_text "Assigned: #{second_user.name}"
+    expect(page.first_project)
+      .not_to have_button("Pick random")
   end
 end
