db313192bfe074cfd1be031b9683763aeca9c18f
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-06 07:15:32 +0200

    spec for checking project

--- spec/features/assigning_person_to_make_check_spec.rb
+++ spec/features/assigning_person_to_make_check_spec.rb
@@ -10,12 +10,27 @@ feature "assign user to perform check" do
           )
   end
   let(:user) { create(:user, uid: "12331", provider: "google_oauth2") }
+  let(:page) { Reminders::ReminderPage.new }
 
   before do
     log_in(user)
   end
 
   scenario "there is no user assigned yet" do
+    page.load reminder_id: reminder.id
+
+    expect(page.first_project)
+      .not_to have_text user.name
+    expect(page.first_project)
+      .to have_button("Pick random")
+
+    page.first_project.pick_random_button.click
+
+    expect(page.first_project.assigned_reviewer)
+      .to have_text user.name
+    expect(page.first_project)
+      .not_to have_button("Pick random")
+  end
     visit reminder_path(reminder)
     expect(page).not_to have_text user.name
     expect(page).to have_button("Pick random")
