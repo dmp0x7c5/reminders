c6b7978618161c3bb566673035458f2a84ea2cb1
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-10 13:06:22 +0200

    update use_case spec

--- spec/use_cases/reminders/sync_projects_spec.rb
+++ spec/use_cases/reminders/sync_projects_spec.rb
@@ -16,6 +16,9 @@ describe Reminders::SyncProjects do
       expect(projects_repo).to receive(:all) { all_projects }
       expect(projects_repo).to receive(:for_reminder)
         .with(reminder) { reminder_projects }
+      all_projects.each do |project|
+        allow(project).to receive(:enabled)
+      end
     end
 
     context "where there is no new projects" do
@@ -28,6 +31,33 @@ describe Reminders::SyncProjects do
 
     it "creates new project checks for each new project" do
       expect { subject.call }.to change { project_checks_repo.all.count }.by(2)
+    end
+
+    context "when one of projects is disabled" do
+      let(:project1_checks) do
+        project_checks_repo.all.select { |pc| pc.project_id == project2.id }
+      end
+      let(:project2_checks) do
+        project_checks_repo.all.select { |pc| pc.project_id == project3.id }
+      end
+
+      before do
+        allow(project2).to receive(:enabled)
+          .and_return(false)
+        allow(project3).to receive(:enabled)
+          .and_return(true)
+        allow(project1).to receive(:project_checks)
+          .and_return(:project1_checks)
+        allow(project2).to receive(:project_checks)
+          .and_return(:project2_checks)
+      end
+
+      it "creates project check with the same state as project" do
+        subject.call
+
+        expect(project1_checks.first.enabled).to be false
+        expect(project2_checks.first.enabled).to be true
+      end
     end
   end
 end
