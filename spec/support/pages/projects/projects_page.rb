94175948590d400652a3f32576b69e969263f5ac
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-10 10:46:21 +0200

    create site_prism projects page

---
+++ spec/support/pages/projects/projects_page.rb
@@ -0,0 +1,11 @@
+require_relative './sections/project_table_row.rb'
+module Projects
+  class ProjectsPage < SitePrism::Page
+    set_url "/projects"
+    sections :project_rows, ProjectTableRow, ".row .col-xs-12 table.table tbody tr.projects_list"
+
+    def first_project
+     project_rows.first
+    end
+  end
+end
