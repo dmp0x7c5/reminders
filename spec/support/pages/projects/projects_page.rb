9b0f989b9fee200572adbaee02252981a06c7b5f
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-10 15:47:19 +0200

    guardfix

--- spec/support/pages/projects/projects_page.rb
+++ spec/support/pages/projects/projects_page.rb
@@ -3,7 +3,8 @@ require_relative "./sections/project_table_row.rb"
 module Projects
   class ProjectsPage < SitePrism::Page
     set_url "/projects"
-    sections :project_rows, ProjectTableRow, ".row .col-xs-12 table.table tbody tr.projects_list"
+    sections :project_rows, ProjectTableRow,
+             ".row .col-xs-12 table.table tbody tr.projects_list"
 
     def first_project
       project_rows.first
