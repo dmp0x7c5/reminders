c3ed3334f28c9d9693f90846d5cd216c27c42202
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-10 14:36:57 +0200

    rename page to reminder_page in test

--- spec/features/assigning_person_to_make_check_spec.rb
+++ spec/features/assigning_person_to_make_check_spec.rb
@@ -10,48 +10,49 @@ feature "assign user to perform check" do
           )
   end
   let(:user) { create(:user, uid: "12331", provider: "google_oauth2") }
-  let(:second_user) { create(:user, name: "John Smith") }
-  let(:page) { Reminders::ReminderPage.new }
+  let!(:second_user) { create(:user, name: "John Smith") }
+  let(:reminder_page) { Reminders::ReminderPage.new }
 
   before do
     log_in(user)
   end
 
   scenario "there is no user assigned yet" do
-    page.load reminder_id: reminder.id
+    reminder_page.load reminder_id: reminder.id
 
-    expect(page.first_project)
+    expect(reminder_page.first_project)
       .not_to have_text user.name
-    expect(page.first_project)
+    expect(reminder_page.first_project)
       .to have_button("Pick random")
 
-    page.first_project.pick_random_button.click
+    reminder_page.first_project.pick_random_button.click
 
-    expect(page.first_project.assigned_reviewer)
+    expect(reminder_page.first_project.assigned_reviewer)
       .to have_text user.name
-    expect(page.first_project)
+    expect(reminder_page.first_project)
       .not_to have_button("Pick random")
   end
 
   scenario "someone has already checked project" do
-    visit reminder_path(reminder)
-    expect(page.first_project)
+    reminder_page.load reminder_id: reminder.id
+
+    expect(reminder_page.first_project.last_checker)
       .not_to have_text user.name
-    expect(page.first_project)
+    expect(reminder_page.first_project)
       .to have_button("Pick random")
 
-    page.first_project.check_button.click
+    reminder_page.first_project.check_button.click
 
-    expect(page.first_project.last_checker)
+    expect(reminder_page.first_project.last_checker)
       .to have_text user.name
-    expect(page.first_project)
+    expect(reminder_page.first_project)
       .not_to have_text second_user.name
 
-    page.first_project.pick_random_button.click
+    reminder_page.first_project.pick_random_button.click
 
-    expect(page.first_project.assigned_reviewer)
+    expect(reminder_page.first_project.assigned_reviewer)
       .to have_text "Assigned: #{second_user.name}"
-    expect(page.first_project)
+    expect(reminder_page.first_project)
       .not_to have_button("Pick random")
   end
 end
