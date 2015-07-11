c38f810d8a9bef1d213b1a01ab052b79e5c0b9c9
Adam Nowak <adam.nowak@netguru.co>
2015-07-11 18:23:52 +0200

    use the correct way of specifying class

--- app/models/project_check.rb
+++ app/models/project_check.rb
@@ -1,7 +1,7 @@
 class ProjectCheck < ActiveRecord::Base
   belongs_to :project
   belongs_to :reminder
-  belongs_to :last_check_user, class: User
+  belongs_to :last_check_user, class_name: "User"
 
   has_many :check_assignments, -> { order created_at: :desc },
            dependent: :destroy
