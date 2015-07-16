35719e2779eb0e7a02ea8ca5545738113ba71f70
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-16 07:33:47 +0200

    pass users_repository when initialising PickPerson class

--- app/use_cases/check_assignments/pick_person.rb
+++ app/use_cases/check_assignments/pick_person.rb
@@ -3,8 +3,8 @@ module CheckAssignments
     attr_reader :users_repository, :latest_checker
     private :users_repository, :latest_checker
 
-    def initialize(latest_checker:)
-      @users_repository = UsersRepository.new
+    def initialize(latest_checker:, users_repository:)
+      @users_repository = users_repository || UsersRepository.new
       @latest_checker = latest_checker
     end
 
