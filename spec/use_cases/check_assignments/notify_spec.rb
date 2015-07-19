e54b4742a0aa78defe4b9c6ea15a114ee02a3394
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-07-19 20:59:16 +0200

    missing spec for 'notify' use case in check assignments

---
+++ spec/use_cases/check_assignments/notify_spec.rb
@@ -0,0 +1,74 @@
+require "spec_helper"
+
+describe CheckAssignments::Notify do
+  let(:service) { described_class.new }
+  let(:message) { "Message." }
+  let(:channel) { "test" }
+
+  describe "#notify" do
+    context "with disabled Slack" do
+      before do
+        allow(AppConfig).to receive(:slack_enabled)
+          .and_return(false)
+      end
+
+      it "returns message of unsuccessful notification" do
+        expect(service.call channel, message)
+          .to eq(
+            "Message. Something went wrong and we couldn't notify channel.",
+          )
+      end
+    end
+
+    context "with enabled Slack" do
+      before do
+        allow(AppConfig).to receive(:slack_enabled)
+          .and_return(true)
+        allow_any_instance_of(Notifier).to receive(:notify_slack)
+          .and_return("ok" => "")
+      end
+
+      it "makes call to Notifier" do
+        expect_any_instance_of(Notifier)
+          .to receive(:notify_slack)
+        service.call(channel, message)
+      end
+
+      context "after successful notification" do
+        let(:notifier_output) do
+          { "ok" => true }
+        end
+
+        before do
+          allow_any_instance_of(Notifier).to receive(:notify_slack)
+            .and_return("ok" => true)
+        end
+
+        it "returns ok message" do
+          expect(service.call channel, message)
+            .to eq(
+              "Message. We have notified project's channel.",
+            )
+        end
+      end
+
+      context "after unsuccessful notification" do
+        let(:notifier_output) do
+          { "ok" => false }
+        end
+
+        before do
+          allow_any_instance_of(Notifier).to receive(:notify_slack)
+            .and_return("ok" => false)
+        end
+
+        it "returns fail message" do
+          expect(service.call channel, message)
+            .to eq(
+              "Message. Something went wrong and we couldn't notify channel.",
+            )
+        end
+      end
+    end
+  end
+end
