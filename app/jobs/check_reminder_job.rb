7e46e538edb3f140c961503d4ee611bbfc5de7b2
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-04 15:53:13 +0200

    Move part of condition to perform method

--- app/jobs/check_reminder_job.rb
+++ app/jobs/check_reminder_job.rb
@@ -5,10 +5,12 @@ class CheckReminderJob
 
   def perform(reminder_id)
     reminder = reminders_repository.find reminder_id
-    checks_for_reminder(reminder).each do |check|
-      ProjectCheckedOnTimeJob.new(check.id,
-                                  reminder.valid_for_n_days,
-                                  reminder.remind_after_days).perform
+    checks_for_reminder(reminder).each do |project_check|
+      if project_check.check_assignments.none? { |c| c.completion_date.nil? }
+        ProjectCheckedOnTimeJob.new(project_check.id,
+                                    reminder.valid_for_n_days,
+                                    reminder.remind_after_days).perform
+      end
     end
   end
 
@@ -24,8 +26,7 @@ class CheckReminderJob
 
   def checks_for_reminder(reminder)
     project_checks_repository.for_reminder(reminder).select do |project_check|
-      project_check.enabled? &&
-        project_check.check_assignments.none? { |c| c.completion_date.nil? }
+      project_check.enabled?
     end
   end
 end
