edc19daedb56b244cc88fea23f6b1e8854478030
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-07 16:27:32 +0200

    Add condition to get only unpaused users in users_with_skill_repository

--- app/repositories/users_with_skill_repository.rb
+++ app/repositories/users_with_skill_repository.rb
@@ -6,6 +6,8 @@ class UsersWithSkillRepository
   end
 
   def all
-    User.joins(:skills).where(skills: { reminder_id: reminder.id })
+    User.joins(:skills)
+      .where(skills: { reminder_id: reminder.id })
+      .where(paused: false)
   end
 end
