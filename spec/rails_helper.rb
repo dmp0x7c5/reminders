624c55495986239270df284468f9d998fa31063d
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-08 08:42:17 +0200

    Include webkit as js driver in specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,6 +19,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
+  Capybara.javascript_driver = :webkit
   config.include RSpecHtmlMatchers
   config.include FeatureHelpers, type: :feature
 
