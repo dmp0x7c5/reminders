84f1acd8663b14afb172f694795b8e70459caf3c
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-08 12:48:34 +0200

    Add feature specs for toggling state of paused users

---
+++ spec/features/pausing_users_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+feature "pausing users" do
+  let(:user) do
+    create(:user,
+           uid: "12331", provider: "google_oauth2",
+           email: "john@doe.pl", admin: true, paused: false)
+  end
+  let(:second_user) do
+    create(:user,
+           name: "John Smith",
+           email: "john@smith@netguru.pl",
+           paused: true)
+  end
+  let(:users_page) { Users::UsersPage.new }
+
+  before do
+    log_in(user)
+    second_user.touch
+  end
+
+  pending "toggling state of paused in '/users' page" do
+    users_page.load
+    expect(users_page.user_rows.first.paused?).to be_falsy
+    expect(users_page.user_rows.second.paused?).to be_truthy
+    users_page.user_rows.first.pause!
+    expect(users_page.user_rows.first.paused?).to be_truthy
+    expect(user.reload.paused).to be_truthy
+    users_page.user_rows.first.unpause!
+    expect(users_page.user_rows.first.paused?).to be_falsy
+  end
+end
--- spec/support/pages/users/sections/users_table_row.rb
+++ spec/support/pages/users/sections/users_table_row.rb
@@ -3,9 +3,22 @@ module Users
     element :admin, ".pick-random-button"
     element :toggle_admin_button, "#toggle-admin-button"
     element :admin_label, ".label.label-primary"
+    element :toggle_paused_button, ".project-switch"
 
     def toggle_admin_permissions!
       toggle_admin_button.click
+    end
+
+    def paused?
+      toggle_paused_button[:checked] != true
+    end
+
+    def pause!
+      toggle_paused_button.set(false)
+    end
+
+    def unpause!
+      toggle_paused_button.set(true)
     end
   end
 end
