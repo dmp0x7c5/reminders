821d52a9cf8fcef28caafcfa0990ee10485a8c35
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-09 08:22:07 +0200

    Add Archive service + specs

---
+++ spec/use_cases/users/archive_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe Users::Archive do
+  let(:service) { described_class.new(user) }
+  let(:user) { create(:user) }
+
+  describe "#call" do
+    it "set current date on archived_at field of passed user" do
+      Timecop.freeze(Time.now) do
+        expect { service.call }.to change { user.archived_at }
+        expect(user.archived_at).to eq(Time.now)
+      end
+    end
+  end
+end
