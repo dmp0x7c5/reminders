00500c2c30a73ea35b71e84450bc204764971fe8
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-09 14:21:43 +0200

    Expose user_check_assignments in visitors_controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,3 +1,8 @@
 class VisitorsController < ApplicationController
   skip_before_action :authenticate_user!
+
+  expose(:user_check_assignments) do
+    assignments_repository.latest_user_assignments(user_id: current_user.id)
+  end
+  expose(:assignments_repository) { CheckAssignmentsRepository.new }
 end
