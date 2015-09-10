33dfb0f831244f21c49dfe95888db65c677565ee
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-10 15:20:42 +0200

    It was moved to service

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,5 @@ class User < ActiveRecord::Base
   has_many :skills
   has_many :check_assignments
 
-  def self.create_with_omniauth(auth)
-    create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.name = auth["info"]["name"] || "" if auth["info"]
-      user.email = auth["info"]["email"] || "" if auth["info"]
-    end
   end
 end
