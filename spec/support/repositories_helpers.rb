cb64bd4577c4c9b70d637fd7e3998f644c7b748f
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-14 12:20:53 +0200

    CheckAssignmentsInMemoryRepository is used only here, so can be defined here + adjust specs

--- spec/support/repositories_helpers.rb
+++ spec/support/repositories_helpers.rb
@@ -4,10 +4,4 @@ module RepositoriesHelpers
     repo.all = all
     repo
   end
-
-  def create_check_assignments_repository(*all)
-    repo = CheckAssignmentsInMemoryRepository.new
-    repo.all = all
-    repo
-  end
 end
--- spec/use_cases/check_assignments/remind_pending_check_assignment_spec.rb
+++ spec/use_cases/check_assignments/remind_pending_check_assignment_spec.rb
@@ -20,32 +20,30 @@ describe CheckAssignments::RemindPendingCheckAssignment do
     double(:project_check, id: 1, created_at: Time.current, reminder_id: 1)
   end
 
-  describe "#perform" do
-    before do
-      allow(check_assignments_repository).to receive(:latest_assignment) do
-        check_assignments_repository.all.last
+  let(:check_assignments_repository) do
+    class CheckAssignmentsInMemoryRepository < InMemoryRepository
+      def latest_assignment(_project_check, completed: false)
+        all.last
       end
     end
+    CheckAssignmentsInMemoryRepository.new
+  end
 
+  describe "#perform" do
     after do
       service.call
     end
 
     context "when check has no one assigned" do
-      let(:check_assignments_repository) { create_check_assignments_repository }
-
       it "doesn't send any reminder" do
         expect(UserReminderMailer).to_not receive(:check_assignment_remind)
       end
     end
 
     context "when check has assigned user today" do
-      let(:check_assignments_repository) do
-        create_check_assignments_repository(check_assignment)
-      end
-
       before do
         allow(check_assignment).to receive(:created_at) { Time.current }
+        check_assignments_repository.all = [check_assignment]
       end
 
       it "doesn't send any reminder" do
@@ -54,14 +52,11 @@ describe CheckAssignments::RemindPendingCheckAssignment do
     end
 
     context "when check has assigned user 15 days ago" do
-      let(:check_assignments_repository) do
-        create_check_assignments_repository(check_assignment)
-      end
-
       before do
         allow(check_assignment).to receive(:created_at) { 15.days.ago }
         allow(UserReminderMailer)
           .to receive_message_chain(:check_assignment_remind, :deliver_now)
+        check_assignments_repository.all = [check_assignment]
       end
 
       it "sends reminder" do
@@ -70,16 +65,13 @@ describe CheckAssignments::RemindPendingCheckAssignment do
     end
 
     context "when check has completion_date" do
-      let(:check_assignments_repository) do
-        create_check_assignments_repository(check_assignment)
-      end
-
       before do
         allow(check_assignment).to receive(:created_at) { 15.days.ago }
         allow(check_assignment).to receive(:completion_date) { Time.current }
 
         allow(UserReminderMailer)
           .to receive_message_chain(:check_assignment_remind, :deliver)
+        check_assignments_repository.all = [check_assignment]
       end
 
       it "doesn't send any reminder" do
