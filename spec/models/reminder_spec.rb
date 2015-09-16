f6d584692bd65c510175e7d33c515c161f51923c
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-16 09:46:25 +0200

    Fix rspec warnings

--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -9,24 +9,26 @@ describe Reminder do
     let!(:check_assignment) do
       create(:check_assignment, project_check: project_check, user: user)
     end
-    let(:skill) { create(:skill, user: user, reminder: reminder) }
+    let!(:skill) { create(:skill, user: user, reminder: reminder) }
 
     before { reminder.destroy }
 
     it "deletes reminder" do
-      expect { reminder.reload }.to raise_error
+      expect { reminder.reload }.to raise_error(ActiveRecord::RecordNotFound)
     end
 
     it "deletes dependent project_checks" do
-      expect { project_check.reload }.to raise_error
+      expect { project_check.reload }
+        .to raise_error(ActiveRecord::RecordNotFound)
     end
 
     it "deletes dependent check_assignments" do
-      expect { check_assignment.reload }.to raise_error
+      expect { check_assignment.reload }
+        .to raise_error(ActiveRecord::RecordNotFound)
     end
 
     it "deletes dependent skills" do
-      expect { skill.reload }.to raise_error
+      expect { skill.reload }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
