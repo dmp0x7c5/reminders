aaeef0d3c1bcd1679ac5a329f1b182eecbe5107b
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-16 09:41:56 +0200

    Destroy dependent objects when destroying reminder + specs

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -3,8 +3,8 @@ class Reminder < ActiveRecord::Base
   validates :deadline_text, :notification_text, presence: true,
                                                 length: { minimum: 10 }
   validates :valid_for_n_days, numericality: true
-  has_many :project_checks
+  has_many :project_checks, dependent: :destroy
   has_many :projects, through: :project_checks
-  has_many :check_assignments, through: :project_checks
-  has_many :skills
+  has_many :check_assignments, through: :project_checks, dependent: :destroy
+  has_many :skills, dependent: :destroy
 end
---
+++ spec/models/reminder_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+describe Reminder do
+  let!(:reminder) { create(:reminder) }
+
+  context "Destroing" do
+    let!(:project_check) { create(:project_check, reminder: reminder) }
+    let(:user) { create(:user) }
+    let!(:check_assignment) do
+      create(:check_assignment, project_check: project_check, user: user)
+    end
+    let(:skill) { create(:skill, user: user, reminder: reminder) }
+
+    before { reminder.destroy }
+
+    it "deletes reminder" do
+      expect { reminder.reload }.to raise_error
+    end
+
+    it "deletes dependent project_checks" do
+      expect { project_check.reload }.to raise_error
+    end
+
+    it "deletes dependent check_assignments" do
+      expect { check_assignment.reload }.to raise_error
+    end
+
+    it "deletes dependent skills" do
+      expect { skill.reload }.to raise_error
+    end
+  end
+end
