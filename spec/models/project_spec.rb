17d5551b8603baac1a5f7b50f6be18c8e84fe98b
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-16 13:21:16 +0200

    No need to instantiate project to test validation
    [10735ced8f723e134212d5fd94307edf5680df3e]

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,8 +2,6 @@ require "rails_helper"
 
 describe Project do
   describe "ActiveModel validations" do
-    let(:project) { create(:project) }
-    it { expect(project).to validate_presence_of(:email) }
-    it { expect(project).to validate_presence_of(:channel_name) }
+    it { is_expected.to validate_presence_of(:email) }
   end
 end
