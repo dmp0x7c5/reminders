bf5904dc392697ef710ccc41fb3b74f206943f8a
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-16 13:21:46 +0200

    Fix
    [1ccf4ec0330b08e6742a1ea83c932bcf006e0e34]

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 describe Project do
   describe "ActiveModel validations" do
     it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:channel_name) }
   end
 end
